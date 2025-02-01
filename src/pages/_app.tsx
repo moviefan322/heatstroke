fe08d76eb261b964e41243e400932607e951b0a4
moviefan322 <philman202@yahoo.com>
2025-02-01 00:09:23 -0500

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Tourney } from "next/font/google";
+
+const tourney = Tourney({
+  variable: "--font-tourney",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={tourney.variable}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
